5c9a5692e6484a85d89c2d7f1f233a6928d090ab
Kris Jenkins <krisajenkins@gmail.com>
2016-10-04 15:32:10 +0100

    Release.

--- interop.js
+++ interop.js
@@ -3,35 +3,27 @@
 window.addEventListener(
     'load',
     function () {
-        var app = Elm.fullscreen(
-            Elm.Main,
-            {
-                orientationSignal : null,
-                orientationErrorSignal : null,
-                geolocationSignal : null,
-                geolocationErrorSignal : null
-            }
-        );
+        var app = Elm.App.fullscreen();
 
         if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(app.ports.geolocationSignal.send);
+            navigator.geolocation.getCurrentPosition(app.ports.geolocation.send);
             navigator.geolocation.watchPosition(
-                app.ports.geolocationSignal.send,
-                app.ports.geolocationErrorSignal.send,
+                app.ports.geolocation.send,
+                app.ports.geolocationError.send,
                 {maximumAge : 1000}
             );
         } else {
-            app.ports.geolocationErrorSignal.send("Device does not support geolocation checks.");
+            app.ports.geolocationError.send("Device does not support geolocation checks.");
         }
 
         if (window.DeviceOrientationEvent) {
             window.addEventListener(
                 'deviceorientation',
-                app.ports.orientationSignal.send,
+                app.ports.orientation.send,
                 false
             );
         } else {
-            app.ports.orientationErrorSignal.send("Device does not support orientation checks.");
+            app.ports.orientationError.send("Device does not support orientation checks.");
         };
     },
     false
